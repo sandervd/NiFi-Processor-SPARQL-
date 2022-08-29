695573c94f243dc57c02721f443e9bf4dff21dc4
Sander Van Dooren <sander@essentialcomplexity.eu>
2022-08-29 19:06:52 +0200

    Add config properties to NiFi SPARQL processor.

--- src/main/java/be/vlaanderen/informatievlaanderen/ldes/processors/LdesTriplestoreMaterialisation.java
+++ src/main/java/be/vlaanderen/informatievlaanderen/ldes/processors/LdesTriplestoreMaterialisation.java
@@ -1,12 +1,11 @@
 package be.vlaanderen.informatievlaanderen.ldes.processors;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.nifi.annotation.behavior.Stateful;
+import org.apache.nifi.annotation.behavior.InputRequirement;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
 import org.apache.nifi.annotation.documentation.Tags;
 import org.apache.nifi.annotation.lifecycle.OnScheduled;
 import org.apache.nifi.components.PropertyDescriptor;
-import org.apache.nifi.components.state.Scope;
 import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.processor.AbstractProcessor;
 import org.apache.nifi.processor.ProcessContext;
@@ -16,6 +15,7 @@ import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.io.InputStreamCallback;
 import org.apache.nifi.processor.io.OutputStreamCallback;
 import org.eclipse.rdf4j.common.exception.RDF4JException;
+import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.repository.manager.RemoteRepositoryManager;
@@ -28,77 +28,62 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.apache.nifi.processor.util.StandardValidators;
-import org.apache.nifi.processor.ProcessorInitializationContext;
 
 @Tags({ "ldes-triplestore, vsds" })
 @CapabilityDescription("Materialises LDES events in a triplestore (SPARQL query)")
-@Stateful(description = "Stores mutable fragments to allow processor restart", scopes = Scope.LOCAL)
+@InputRequirement(InputRequirement.Requirement.INPUT_REQUIRED)
 public class LdesTriplestoreMaterialisation extends AbstractProcessor {
-	private List<PropertyDescriptor> properties;
-	private Set<Relationship> relationships;
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(LdesTriplestoreMaterialisation.class);
 
-	private RepositoryManager repositoryManager = new RemoteRepositoryManager("http://graphdb:7200");
-	public static final PropertyDescriptor REPOSITORY_CONFIG = new PropertyDescriptor.Builder()
-			.name("REPO_CONFIG")
-			.displayName("Repository configuration")
+	private RepositoryManager repositoryManager;
+
+	static final Relationship REL_SUCCESS = new Relationship.Builder()
+			.name("success")
+			.description("A FlowFile is routed to this relationship after the database is successfully updated")
+			.build();
+	static final Relationship REL_RETRY = new Relationship.Builder()
+			.name("retry")
+			.description("A FlowFile is routed to this relationship if the database cannot be updated but attempting the operation again may succeed")
+			.build();
+	static final Relationship REL_FAILURE = new Relationship.Builder()
+			.name("failure")
+			.description("A FlowFile is routed to this relationship if the database cannot be updated and retrying the operation will also fail, "
+					+ "such as an invalid query or an integrity constraint violation")
+			.build();
+
+	static final PropertyDescriptor SPARQL_HOST = new PropertyDescriptor.Builder()
+			.name("SPARQL host")
+			.description("The hostname and port of the SPARQL server.")
+			.defaultValue("http://graphdb:7200")
+			.required(true)
+			.addValidator(StandardValidators.URL_VALIDATOR)
+			.build();
+
+	static final PropertyDescriptor REPOSITORY_ID = new PropertyDescriptor.Builder()
+			.name("Repository ID")
+			.description("The repository to connect to.")
 			.required(true)
 			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
 			.build();
 
-	public static final PropertyDescriptor REPOSITORY_ID = new PropertyDescriptor.Builder()
-			.name("REPO_ID")
-			.displayName("Repository ID")
-			.required(true)
-			.addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+	static final PropertyDescriptor NAMED_GRAPH = new PropertyDescriptor.Builder()
+			.name("Named graph")
+			.description("If set, the named graph the triples will be written to.")
+			.required(false)
+			.addValidator(StandardValidators.URI_VALIDATOR)
 			.build();
 
 
-	public static final Relationship SUCCESS = new Relationship.Builder()
-			.name("SUCCESS")
-			.description("Succes relationship")
-			.build();
-
-	@Override
-	public void onPropertyModified(PropertyDescriptor descriptor, String oldValue, String newValue) {
-		if (descriptor.getName().equals("REPO_CONFIG")) {
-			this.getLogger().debug("(Re-)loading repository config.");
-			//SPARQLRepositoryConfig sparqlRepoConfig = new SPARQLRepositoryConfig();
-			//sparqlRepoConfig.setQueryEndpointUrl("http://graphdb:7200/sparql");
-			//sparqlRepoConfig.setUpdateEndpointUrl("http://graphdb:7200/sparql");
-			//RepositoryConfig repConfig = new RepositoryConfig("sparql_repository", sparqlRepoConfig);
-			//this.repositoryManager.addRepositoryConfig(repConfig);
-		}
-	}
-
-	@Override
-	public void init(final ProcessorInitializationContext context) {
-		List<PropertyDescriptor> properties = new ArrayList<>();
-		//properties.add(REPOSITORY_CONFIG);
-		//properties.add(REPOSITORY_ID);
-		this.properties = Collections.unmodifiableList(properties);
-
-		Set<Relationship> relationships = new HashSet<>();
-		relationships.add(SUCCESS);
-		this.relationships = Collections.unmodifiableSet(relationships);
-
-		// Repository manager
-		this.repositoryManager.init();
-
-		//throw new RuntimeException("Scheduling not implemented");
-
-	}
 	@OnScheduled
 	public void onScheduled(final ProcessContext context) {
-
+		this.repositoryManager = new RemoteRepositoryManager(context.getProperty(SPARQL_HOST).getValue());
 	}
 
 	@Override
 	public void onTrigger(ProcessContext context, ProcessSession session) throws ProcessException {
-		final AtomicReference<String> value = new AtomicReference<>();
 		FlowFile flowfile = session.get();
 
 		if(flowfile == null) {
@@ -106,17 +91,28 @@ public class LdesTriplestoreMaterialisation extends AbstractProcessor {
 			return;
 		}
 
-		Repository repository = this.repositoryManager.getRepository("vsds");
-
 		session.read(flowfile, new InputStreamCallback() {
+
 			@Override
 			public void process(InputStream in) throws IOException {
 					String FragmentRDF = IOUtils.toString(in);
 					getLogger().debug(String.format("Got the following RDF: %s", FragmentRDF));
+					Repository repository = repositoryManager.getRepository(context.getProperty(REPOSITORY_ID).getValue());
+					String namedGraph = context.getProperty(NAMED_GRAPH).getValue();
+
 					try (RepositoryConnection con = repository.getConnection()) {
+
 						InputStream targetStream = IOUtils.toInputStream(FragmentRDF);
 						con.begin();
-						con.add(targetStream, RDFFormat.N3);
+						if (namedGraph.isEmpty()) {
+							con.add(targetStream, RDFFormat.N3);
+						}
+						else {
+							IRI namedGraphIRI = con.getValueFactory().createIRI(namedGraph);
+							con.add(targetStream, RDFFormat.N3, namedGraphIRI);
+						}
+
+
 						con.commit();
 					}
 					catch (RDF4JException e) {
@@ -125,12 +121,6 @@ public class LdesTriplestoreMaterialisation extends AbstractProcessor {
 					}
 			}
 		});
-
-		// Write the results to an attribute
-		String results = value.get();
-		if(results != null && !results.isEmpty()){
-			flowfile = session.putAttribute(flowfile, "match", results);
-		}
 
 		// To write the results back out ot flow file
 		flowfile = session.write(flowfile, new OutputStreamCallback() {
@@ -141,15 +131,23 @@ public class LdesTriplestoreMaterialisation extends AbstractProcessor {
 			}
 		});
 
-		session.transfer(flowfile, SUCCESS);
+		session.transfer(flowfile, REL_SUCCESS);
 	}
 	@Override
-	public Set<Relationship> getRelationships(){
-		return relationships;
+	public Set<Relationship> getRelationships() {
+		final Set<Relationship> rels = new HashSet<>();
+		rels.add(REL_SUCCESS);
+		rels.add(REL_RETRY);
+		rels.add(REL_FAILURE);
+		return rels;
 	}
 
 	@Override
-	public List<PropertyDescriptor> getSupportedPropertyDescriptors(){
+	protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+		final List<PropertyDescriptor> properties = new ArrayList<>();
+		properties.add(SPARQL_HOST);
+		properties.add(REPOSITORY_ID);
+		properties.add(NAMED_GRAPH);
 		return properties;
 	}
 }
